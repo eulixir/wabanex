16be989aa5674ac04ddeecf114d0d9a606701703
João Pedro Alves <jotalmeida007@hotmail.com>
2021-06-22 20:02:52 -0300

    Aula 3

---
+++ lib/wabanex_web/resolvers/user.ex
@@ -0,0 +1,6 @@
+defmodule WabanexWeb.Resolver.User do
+  alias Wabanex.Users
+
+  def create(%{input: params}, _context), do: Users.Create.call(params)
+  def get(%{id: user_id}, _context), do: Users.Get.call(user_id)
+end
